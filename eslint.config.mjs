74fd509cfa35b11e99a3f578939b4bd10c2f640d
moses <okonkwomoses158@gmail.com>
2026-06-17 14:31:41 +0100

    chore: disable strict TypeScript and React linting rules in eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,15 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "prefer-const": "off",
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
